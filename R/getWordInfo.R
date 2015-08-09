c1d054d460b1ff0f93852dfc816ab7fa04d2ff9e
Brian Loyal <bloyal@gmail.com>
2015-08-09 09:57:13 -0500

    fixed no-null error in get word info

--- R/getWordInfo.R
+++ R/getWordInfo.R
@@ -27,12 +27,13 @@ getWordInfo <- function(graph, word) {
   return a.name as word, b.posName as pos, r.wordNum as sense, b.gloss as gloss, b.lexFileName as lexFile, r2.pointerType as pointer, r2.relation as pointerType, d.name as pointerWord
   order by a.name, b.posName, r.wordNum, r2.pointerType, r3.wordNum"
   semanticResults <- unique(cypher(graph, query, word = word))
-  semanticResults <- merge(semanticResults, plyr::ddply(semanticResults, plyr::.(pos,sense,pointer),
-                                                        plyr::summarise, pointerWords = paste0(pointerWord,
-                                                        collapse = ", ")), by = c("pos", "sense", "pointer"))
-  results <- unique(semanticResults[, c("word", "pos", "sense", "gloss",
-                                        "lexFile", "pointer", "pointerType", "pointerWords")])
-
+  if (!is.null(semanticResults)) {
+    semanticResults <- merge(semanticResults, plyr::ddply(semanticResults, plyr::.(pos,sense,pointer),
+                                                          plyr::summarise, pointerWords = paste0(pointerWord,
+                                                          collapse = ", ")), by = c("pos", "sense", "pointer"))
+    results <- unique(semanticResults[, c("word", "pos", "sense", "gloss",
+                                          "lexFile", "pointer", "pointerType", "pointerWords")])
+  }
   # get lexical pointer info
   query <- "match (a:Word {name:{word}})-[r:has_synset]->(b:Synset),\n          (a)-[r2:has_pointer]->(c:Word)\n            return a.name as word, b.posName as pos,r.wordNum as sense, b.gloss as gloss, b.lexFileName as lexFile, r2.pointerType as pointer, r2.relation as pointerType, c.name as pointerWord\n            order by a.name, b.posName,r.wordNum, r2.pointerType"
   lexResults <- unique(cypher(graph, query, word = word))
@@ -44,6 +45,11 @@ getWordInfo <- function(graph, word) {
                                         "lexFile", "pointer", "pointerType", "pointerWords")])
     results <- rbind(results, lexResults)
   }
-  return(results[order(results$word, results$pos, results$sense, results$pointerType,
+  if (is.null(semanticResults) & is.null(lexResults)){
+    return(NULL)
+    }
+  else{
+    return(results[order(results$word, results$pos, results$sense, results$pointerType,
                        results$pointer), ])
+  }
 }
